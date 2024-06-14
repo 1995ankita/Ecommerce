ff570a09c54f7f69f1dcfe6c07b00d285daef5ca
1995ankita <ankita.hirpara@theknowledgeacademy.com>
2024-06-14 18:37:35 +0530

    product module

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -52,10 +52,51 @@ const getaproduct = asyncHandler(async (req, res) => {
 
 const getallproduct = asyncHandler(async (req, res) => {
     try {
-        const getallProducts = await Product.find();
-        res.json(getallProducts);
+        //Filtering
+        const queryObj = { ...req.query };
+        const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach((el) => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        //Sorting 
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort("createdAt");
+        }
+        //limiting the fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join("");
+            query = query.sort(fields);
+        } else {
+            query = query.select("-__v");
+        }
+
+        // pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+        }
+
+
+
+
+
+        const product = await query;
+        res.json(product);
+
+        // const getallProducts = await Product.find();
+        // res.json(getallProducts);
     } catch (error) {
         throw new Error(error);
     }
 });
-module.exports = { createProduct, getaproduct, getallproduct, updateProduct,deleteProduct };
+module.exports = { createProduct, getaproduct, getallproduct, updateProduct, deleteProduct };
